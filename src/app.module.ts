a0d5f353225de3606da9c810574b4183b7cb35e5
tdremon <emontdr@gmail.com>
2024-06-02 16:41:26 +0900

    chapter3 practice code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ApiController } from './api/api.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule],
+  // 3.1.8 하위 도메인 라우팅
+  // ApiController가 먼저 처리되도록 순서를 수정
+  controllers: [ApiController, AppController],
   providers: [AppService],
 })
 export class AppModule {}
